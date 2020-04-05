8bd410e46f4386f714d9f1eda8105e424b68ece9
foundlios <foundlioss@gmail.com>
2020-04-06 02:22:28 +0700

    PNL-QR

---
+++ app/src/main/java/com/example/mt4/activity/git_test.java
@@ -0,0 +1,16 @@
+package com.example.mt4.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mt4.R;
+
+public class git_test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_git_test );
+    }
+}
